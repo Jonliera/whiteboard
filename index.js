93ece8c04c5743e97cad9b1e008ee501fa042eed
Jonathan Liera <jonliera1@gmail.com>
2023-06-13 21:24:48 -0700

    add functions to the whiteboard

--- index.js
+++ index.js
@@ -127,3 +127,50 @@ function reverseWords(str) {
 }
 
 reverseWords("hello world"); // world hello
+
+//Fibonacci sequence
+
+//11. Write a function that takes a number n and returns the nth number in the Fibonacci sequence.
+
+function fib(n) {
+  if (n <= 1) return n;
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib(0); // 0
+fib(1); // 1
+fib(2); // 1
+fib(3); // 2
+fib(4); // 3
+fib(5); // 5
+fib(6); // 8
+fib(7); // 13
+fib(8); // 21
+fib(9); // 34
+fib(10); // 55
+
+//13. Fizzbuzz
+
+// Write a function that does the following:
+
+// console logs the numbers from 1 to n, where n is the integer the function takes as its parameter
+
+// logs fizz instead of the number for multiples of 3
+
+// logs buzz instead of the number for multiples of 5
+
+// logs fizzbuzz for numbers that are multiples of both 3 and 5
+
+function fizzbuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
